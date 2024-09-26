854797f191d4c6d2aac51be0761fbbeb63da11c7
DmitriyEliseev <93958489+DmitriyEliseev@users.noreply.github.com>
2024-09-26 10:32:41 +0300

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -3,6 +3,10 @@ class House:
         self.name = name
         self.number_of_floors = number_of_floors
 
+    def validate_type(self, other):
+        if not isinstance(other, type(self)):
+            raise TypeError("Аргумент должен быть объектом класса House")
+
     def go_to(self, new_floor):
         floor = 0
         if new_floor > self.number_of_floors or new_floor < 1:
@@ -21,27 +25,27 @@ class House:
 
     def __eq__(self, other):
 
-        return h1.number_of_floors == h2.number_of_floors
+        return self.number_of_floors == other.number_of_floors
 
     def __lt__(self, other):
 
-        return h1.number_of_floors < h2.number_of_floors
+        return self.number_of_floors < other.number_of_floors
 
     def __le__(self, other):
 
-        return  h1.number_of_floors <= h2.number_of_floors
+        return  self.number_of_floors <= other.number_of_floors
 
     def __gt__(self, other):
 
-        return h1.number_of_floors > h2.number_of_floors
+        return self.number_of_floors > other.number_of_floors
 
     def __ge__(self, other):
 
-        return h1.number_of_floors >= h2.number_of_floors
+        return self.number_of_floors >= other.number_of_floors
 
     def __ne__(self, other):
 
-        return h1.number_of_floors != h2.number_of_floors
+        return self.number_of_floors != other.number_of_floors
 
     def __add__(self, other):
         if isinstance(other, int):
@@ -49,10 +53,7 @@ class House:
         return self
 
     def __radd__(self, other):
-        if isinstance(other, int):
-            self.number_of_floors += other
-        return self
-
+        return self.__add__(other)
 
 
 h1 = House('ЖК Эльбрус', 10)
